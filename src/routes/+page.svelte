3b3b54521a4ddf58702832218d0ad0e3ab97f7c6
Josh <jsbusiness305@gmail.com>
2025-02-11 11:39:23 -0500

    added a Our Sponsors section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,33 @@
 <script lang="ts">
-	import logo from '$lib/assets/security.jpg';
-
-	let userForm: { name: string; email: string, message: string } = $state({
+	import { LockKeyholeOpen, ShieldCheck, Cable, Focus } from 'lucide-svelte';
+	let userForm: { name: string; email: string; message: string } = $state({
 		name: '',
 		email: '',
 		message: ''
 	});
 
 	const reasons = [
-		'Security Awareness Training',
-		'Penetration Testing',
-		'Security Consulting',
-		'Malware Analysis'
+		{
+			name: 'Security Awareness Training',
+			icon: ShieldCheck
+		},
+		{
+			name: 'Penetration Testing',
+			icon: Cable
+		},
+		{
+			name: 'Security Consulting',
+			icon: Focus
+		},
+		{
+			name: 'Malware Analysis',
+			icon: LockKeyholeOpen
+		}
 	];
 
 	const handleSubmit = (e: Event) => {
 		e.preventDefault();
-		console.log("form details : ", userForm);
+		console.log('form details : ', userForm);
 		//clear the form
 		userForm = { name: '', email: '', message: '' };
 	};
@@ -27,11 +38,11 @@
 		<div
 			class="absolute -top-50 -left-100 z-[-1] h-[60.2rem] w-[60.2rem] rounded-full border-10 border-yellow-500 bg-black opacity-[5%]"
 		></div>
-		
+
 		<div
 			class="absolute top-10 -left-50 z-[-1] h-[30.2rem] w-[30.2rem] rounded-full border-10 border-yellow-500 bg-black opacity-[5%]"
 		></div>
-		
+
 		<div class="flex flex-col gap-[0.75rem] text-6xl text-white">
 			<span>Welcome</span><span>to</span><span class="font-bold text-orange-500">Cyber Detect</span>
 		</div>
@@ -48,8 +59,7 @@
 
 	<!-- why choose us -->
 	<section
-		
-		class="flex flex-col gap-4 rounded-lg border-2 border-orange-500 p-4 slide-in-from-right"
+		class="slide-in-from-right flex flex-col gap-4 rounded-lg border-2 border-orange-500 p-4"
 	>
 		<h1 class="text-4xl font-bold text-white">Why Choose Us ?</h1>
 		<p>
@@ -59,15 +69,22 @@
 		<div class="flex-col justify-center p-3 sm:flex sm:gap-10 lg:flex-row lg:flex-wrap lg:gap-5">
 			{#each reasons as reason}
 				<div
-					class="flex flex-row items-center justify-center gap-4 rounded-lg bg-neutral-900 p-5 transition duration-300 ease-in-out hover:bg-neutral-700 sm:w-[100%] lg:h-[10rem] lg:w-[35rem]"
+					class="group flex flex-row items-center justify-center gap-4 rounded-lg bg-neutral-900 p-5 transition duration-300 ease-in-out hover:bg-neutral-700 sm:w-[100%] lg:h-[10rem] lg:w-[35rem]"
 				>
-					<img src={logo} alt="security" class="h-[5rem] w-[5rem] rounded-full" />
-					<span class="flex-1 text-white sm:text-sm">{reason}</span>
+					<reason.icon size="5rem" class="text-white group-hover:scale-105 transition duration-300 ease-in" />
+					<span class="flex-1 text-white sm:text-sm">{reason.name}</span>
 				</div>
 			{/each}
 		</div>
 	</section>
 
+
+	<!-- our sponsors -->
+	 <section class=" relative flex min-h-[30rem] flex-col items-center justify-center gap-2 p-4">
+		<h1 class="text-4xl font-bold text-white">Our Sponsors</h1>
+	 </section>
+
+	<!-- form -->
 	<section class=" relative flex min-h-[30rem] flex-col items-center justify-center gap-2 p-4">
 		<div
 			class="absolute top-40 -right-30 z-[-1] h-[10.2rem] w-[10.2rem] rounded-full border-10 border-yellow-500 bg-black opacity-[5%]"
@@ -81,7 +98,7 @@
 		></div>
 
 		<h1 class="text-4xl font-bold text-white">Conect with us</h1>
-		<form onsubmit={handleSubmit}  class="flex flex-col gap-4">
+		<form onsubmit={handleSubmit} class="flex flex-col gap-4">
 			<label for="name" class="text-2xl">Name</label>
 			<input
 				type="text"
@@ -101,7 +118,12 @@
 				class="rounded-lg border-2 p-2"
 			/>
 			<label for="message" class="text-2xl">Message</label>
-			<textarea class="rounded-lg border-2 p-2" name="message" id="message" bind:value={userForm.message}></textarea>
+			<textarea
+				class="rounded-lg border-2 p-2"
+				name="message"
+				id="message"
+				bind:value={userForm.message}
+			></textarea>
 			<button
 				disabled={!userForm.name || !userForm.email}
 				type="submit"
